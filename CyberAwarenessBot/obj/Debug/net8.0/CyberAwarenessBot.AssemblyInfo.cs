b36f92a3d43cbbcbdd12b7d6ae6fc579a3262f3d
ntok-29 <st10376406@rcconnect.edu.za>
2026-04-13 22:34:26 +0200

    User interaction and personalised response added.

--- CyberAwarenessBot/obj/Debug/net8.0/CyberAwarenessBot.AssemblyInfo.cs
+++ CyberAwarenessBot/obj/Debug/net8.0/CyberAwarenessBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075cec39988fbebd28a806e724a001581166c2bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c57dbe30f1b695e6fc5a18624f84cce31859c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
